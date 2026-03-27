7e858e529e8b0faa1feaae74902e5cc0e0df9a4c
kingstonpillar <kingstonpillar@googlemail.com>
2026-03-27 07:15:10 +0100

    Add files via upload

---
+++ tokens.js
@@ -0,0 +1,43 @@
+export const TOKENS = {
+  WETH: {
+    symbol: "WETH",
+    address: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+    decimals: 18,
+  },
+
+  USDC: {
+    symbol: "USDC",
+    address: "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+    decimals: 6,
+  },
+
+  USDT: {
+    symbol: "USDT",
+    address: "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+    decimals: 6,
+  },
+
+  DAI: {
+    symbol: "DAI",
+    address: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+    decimals: 18,
+  },
+
+  WBTC: {
+    symbol: "WBTC",
+    address: "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
+    decimals: 8,
+  },
+
+  ARB: {
+    symbol: "ARB",
+    address: "0x912ce59144191c1204e64559fe8253a0e49e6548",
+    decimals: 18,
+  },
+
+  LINK: {
+    symbol: "LINK",
+    address: "0xf97f4df75117a78c1a5a0dbb814af92458539fb4",
+    decimals: 18,
+  },
+};
