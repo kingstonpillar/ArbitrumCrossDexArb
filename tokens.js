7da093b7e688701e8cff90c32d66f2430aa0b76f
kingstonpillar <kingstonpillar@googlemail.com>
2026-03-28 17:15:53 +0100

    Add files via upload

---
+++ tokens.js
@@ -0,0 +1,79 @@
+export const TOKENS = {
+  WETH: {
+    symbol: "WETH",
+    address: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+    decimals: 18,
+  },
+
+  USDC: {
+    symbol: "USDC",
+    address: "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+    decimals: 6,
+  },
+
+  USDT: {
+    symbol: "USDT",
+    address: "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+    decimals: 6,
+  },
+
+  DAI: {
+    symbol: "DAI",
+    address: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+    decimals: 18,
+  },
+
+  WBTC: {
+    symbol: "WBTC",
+    address: "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
+    decimals: 8,
+  },
+
+  ARB: {
+    symbol: "ARB",
+    address: "0x912ce59144191c1204e64559fe8253a0e49e6548",
+    decimals: 18,
+  },
+
+  LINK: {
+    symbol: "LINK",
+    address: "0xf97f4df75117a78c1a5a0dbb814af92458539fb4",
+    decimals: 18,
+  },
+
+  GMX: {
+    symbol: "GMX",
+    address: "0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a",
+    decimals: 18,
+  },
+
+  RDNT: {
+    symbol: "RDNT",
+    address: "0x3082cc23568ea640225c2467653db90e9250aaa0",
+    decimals: 18,
+  },
+
+  MAGIC: {
+    symbol: "MAGIC",
+    address: "0x539bde0d7dbd336b79148aa742883198bbf60342",
+    decimals: 18,
+  },
+
+  GRAIL: {
+    symbol: "GRAIL",
+    address: "0x3d9907f9a368ad0a51be60f7da3b97cf940982d8",
+    decimals: 18,
+  },
+
+  UNI: {
+    symbol: "UNI",
+    address: "0xfa7f8980b0f1e64a2062791cc3b0871572f1f7f0",
+    decimals: 18,
+  },
+
+  AAVE: {
+    symbol: "AAVE",
+    address: "0xba5ddd1f9d7f570dc94a51479a000e3bce967196",
+    decimals: 18,
+  },
+};
